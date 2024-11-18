cfa5fd25b8fcc28d96f7d9b8a5865dcbeba3f7af
Antoine Kerbiriou <antoine.kerbiriou@sinay.fr>
2024-11-18 14:47:12 +0100

    more verif

--- TP1/Member.py
+++ TP1/Member.py
@@ -14,8 +14,11 @@ class Member:
             
     def return_book(self, book: Book) -> None:
         book.return_book()
-        self.borrowed_books.remove(book)
-        print(f"'{self.name}' has returned '{book}'")
+        if book in self.borrowed_books:
+            self.borrowed_books.remove(book)
+            print(f"'{self.name}' has returned '{book}'")
+        else:
+            print(f"'{self.name}' cant returne '{book}' because it isn't currently borrowed")
         
     def list_borrowed_books(self) -> None:
         if len(self.borrowed_books) > 0:
